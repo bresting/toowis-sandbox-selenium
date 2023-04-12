340036abbb0a2c31316c577b08f330f2686c6cbe
jakekim84 <jakekim84@lfpartners.co.kr>
2023-04-12 10:14:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Selenium + ChromeDriver
+<img src="./files/2023-04-12-01.png" width="200px" height="150px"/>
+<img src="./files/2023-04-12-02.png" />
+
+### 드라이버 + 라이브러리 다운로드
+https://chromedriver.chromium.org/downloads   
+https://www.selenium.dev/downloads/   
+https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-server   
+
+<img src="./files/2023-04-12-03.png" />
+<img src="./files/2023-04-12-04.png" />
+<img src="./files/2023-04-12-05.png" />
+
+chromedriver는 크롬과 통신 하기 위해 사용된다.   
+selenium은 java 라이브러리로 직접 사이트 가서 받거나 maven으로 가져온다.
---
+++ src/main/java/com/toowis/Run.java
@@ -0,0 +1,40 @@
+package com.toowis;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Run {
+    
+    public static final String WEB_DRIVER_ID   = "webdriver.chrome.driver"; // 드라이버 ID
+    public static final String WEB_DRIVER_PATH = Run.class.getClassLoader().getResource("chromedriver.exe").getFile(); // 드라이버 경로
+    
+    public static void main(String[] args) {
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+        
+        ChromeOptions options = new ChromeOptions();
+        
+        // 브라우저 보이지 않기
+        // options.addArguments("headless");
+        
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+        
+        driver.findElement(By.name("q")).sendKeys("java");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        
+        // 브라우저 닫기
+        if (driver != null) {
+            try {
+                Thread.sleep(1000 * 1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            driver.close();
+            driver.quit();
+        }
+    }
+}
